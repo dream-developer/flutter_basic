a697530f30329f7c46004f44234eaed6ba956cde
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 15:14:36 +0900

    基本 - オーバーフロー対策

--- lib/env.dart
+++
@@ -1,9 +0,0 @@
-import 'package:envied/envied.dart'; // 1 
-
-part 'env.g.dart'; // 2
-
-@Envied(path: '.env') // 3
-abstract class Env {
-  @EnviedField(varName: 'API_KEY', obfuscate: true) // 4
-  static final String apikey = _Env.apikey; // 5
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
 
-import 'env.dart'; // 1
-
 void main() {
-  final apikey = Env.apikey; // 2
 
-  final body = SafeArea( 
-    child: Text("APIキー：$apikey"), // 3
-  );
+  const body = Row(children: [
+    Text("あいうえおかきくけこさしすせそ"),
+    Text("アイウエオカキクケコサシスセソ"),
+  ],);
 
-  final sc = Scaffold( body: body);
-  final app = MaterialApp(home: sc);
+  const body1 = Row(children: [
+    Text("あいうえおかきくけこさしすせそ"),
+     Flexible(
+        fit: FlexFit.tight,
+        // fit: FlexFit.loose,
+        child: FittedBox(
+          child: Text("アイウエオカキクケコサシスセソ"),
+        ),
+      ), 
+  ],);
+
+  const body2 = Row(children: [
+    Text("あいうえおかきくけこさしすせそ"),
+    SizedBox(
+      width: 70,
+      child: Text("アイウエオカキクケコサシスセソ"),
+    ),
+  ],);
+
+  const body3 = Row(children: [
+    Text("あいうえおかきくけこさしすせそ"),
+    SizedBox(
+      width: 70,
+      child: Text( 
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+        'アイウエオカキクケコサシスセソ'
+    ),
+    ),
+  ],);
+
+  // 対策パターンにより body1 - body3 を切り替えてください
+  final sc = Scaffold( body: body,);
+  // final sc = Scaffold( body: body1,);
+  // final sc = Scaffold( body: body2,);
+  // final sc = Scaffold( body: body3,);
+  final app = MaterialApp( debugShowCheckedModeBanner: false, home: sc);
   runApp(app);
 }
