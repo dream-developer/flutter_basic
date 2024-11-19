87f2021850212759bbb6da3b150ddfffa09da4a9
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 14:02:10 +0900

    基本 - リストビュー - ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  List<Text> texts = []; // 1
+class User { // Userクラス
+  final int id; // ユーザーID
+  final String name; // ユーザー名
+  User(this.id, this.name);
+}
 
-  for (int i = 1; i <= 50; i++) { // 2
-    texts.add(Text('テキスト$i'));
+List<User> users = []; // User型のList 
+
+void main() {
+  for (int i = 1; i <= 50; i++) { // Listに50個分要素を追加。
+    users.add(User(i, 'ユーザー$i'));
   }
-    
-  final list = ListView.builder( // 3
-    itemCount: texts.length, // 4
-    itemBuilder: (c, i) => texts[i], // 5
+
+  final list = ListView.builder(
+    itemCount: users.length,
+    itemBuilder: (c, i) => 
+      ListTile( // 1
+        key: Key((users[i].id).toString()), // 2
+        leading: const Icon(Icons.person), // 3
+        title: Text(users[i].name), // 4
+        subtitle: Text("id:${users[i].id}"), // 5
+        trailing: const Icon(Icons.edit), // 6
+    ),
   );
 
   final body = Column( // ボディー
     children: [
-      const Text('リストの上'), // 6．
-      Expanded( // 7．
+      const Text('リストの上'), 
+      Expanded( 
         child: list,
       ),
-      const Text('リストの下'), // 6．
+      const Text('リストの下'), 
     ],
   );
 
