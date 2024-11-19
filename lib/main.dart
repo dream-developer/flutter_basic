33ccfc4bba252f7042614f91cd251f8a727c44c9
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 14:33:38 +0900

    基本 - URLを起動する（ブラウザ/メール/電話/SMS など）

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart'; // 1 
+import 'package:url_launcher/url_launcher.dart'; // 1
+
+Future<void> myLaunchUrl(Uri uri) async { // 2
+  if (!await launchUrl(uri)) {
+    throw Exception('URIの起動が出来ません $uri');
+  }
+}
 
 void main() {
-  final button1 = ElevatedButton( 
-    onPressed: () {
-       Fluttertoast.showToast(msg: "トースト"); // 2
-    },
-    child: const Text("トースト"),
+  final uriHttp = Uri.parse('http://example.com');// 3
+  final uriMailto = Uri.parse('mailto:test@example.com?subject=件名&body=本文'); 
+  final uriTel = Uri.parse('tel:+00-00-0000-0000');
+  final uriSms = Uri.parse('sms:000-0000-0000');
+
+  final buttonHttp = ElevatedButton( // 4
+    onPressed: () { myLaunchUrl(uriHttp); },
+    child: const Text("Webアクセス"),
+  );
+  final buttonMailto = ElevatedButton(
+    onPressed: () { myLaunchUrl(uriMailto); },
+    child: const Text("メール"),
+  );
+  final buttonTel = ElevatedButton(
+    onPressed: () { myLaunchUrl(uriTel); },
+    child: const Text("電話"),
+  );
+  final buttonSms = ElevatedButton(
+    onPressed: () { myLaunchUrl(uriSms); },
+    child: const Text("SMS"),
   );
 
-  final button2 = ElevatedButton( 
-    onPressed: () {
-       Fluttertoast.showToast( // 3
-        msg: "長いトースト",
-        toastLength: Toast.LENGTH_LONG,
-      ); 
-    },
-    child: const Text("長いトースト"),
+  final body = SafeArea( // 5
+      child: Column(
+      children: [ buttonHttp, buttonMailto, buttonTel, buttonSms, ],
+    ),
   );
 
   final sc = Scaffold(
-    body: Column( children: [ button1, button2, ],),
+    body: body,
   );
 
   final app = MaterialApp(home: sc);
