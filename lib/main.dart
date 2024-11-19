90fb3f2586c5b9e387db92384c8c2e0940d664a4
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 14:35:54 +0900

    基本 - セーフエリア

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart'; // 1
-
-Future<void> myLaunchUrl(Uri uri) async { // 2
-  if (!await launchUrl(uri)) {
-    throw Exception('URIの起動が出来ません $uri');
-  }
-}
 
 void main() {
-  final uriHttp = Uri.parse('http://example.com');// 3
-  final uriMailto = Uri.parse('mailto:test@example.com?subject=件名&body=本文'); 
-  final uriTel = Uri.parse('tel:+00-00-0000-0000');
-  final uriSms = Uri.parse('sms:000-0000-0000');
+  // SafeAreaなし
+  const body = Text("テキスト");
+  // SafeAreaあり
+  // const body = SafeArea( 
+  //   child: Text("テキスト"),
+  // );
 
-  final buttonHttp = ElevatedButton( // 4
-    onPressed: () { myLaunchUrl(uriHttp); },
-    child: const Text("Webアクセス"),
-  );
-  final buttonMailto = ElevatedButton(
-    onPressed: () { myLaunchUrl(uriMailto); },
-    child: const Text("メール"),
-  );
-  final buttonTel = ElevatedButton(
-    onPressed: () { myLaunchUrl(uriTel); },
-    child: const Text("電話"),
-  );
-  final buttonSms = ElevatedButton(
-    onPressed: () { myLaunchUrl(uriSms); },
-    child: const Text("SMS"),
-  );
-
-  final body = SafeArea( // 5
-      child: Column(
-      children: [ buttonHttp, buttonMailto, buttonTel, buttonSms, ],
-    ),
-  );
-
-  final sc = Scaffold(
+  const sc = Scaffold(
     body: body,
   );
 
-  final app = MaterialApp(home: sc);
+  const app = MaterialApp(home: sc);
   runApp(app);
 }
