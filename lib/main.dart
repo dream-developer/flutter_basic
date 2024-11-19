37c9319c8f3144f7e8260c4e10ea5e27c6fce417
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 14:13:43 +0900

    基本 - 非同期処理 - FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,53 @@
 import 'package:flutter/material.dart';
 
-Future<String> dataFetch() async { // 1
-    await Future.delayed(const Duration(seconds: 1)); // 2
-    return "データ"; // 3
+void main() {
+  runApp(const MyApp());
 }
 
-void main() {
-  final button = ElevatedButton( 
-    onPressed: () async{ // 4
-      final data = await dataFetch();
-      print(data); // 5
-    },
-    child: const Text("ボタン"),
-  );
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  final sc = Scaffold(body: button);
-  final app = MaterialApp(home: sc);
-  runApp(app);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget { 
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState(); 
+}
+
+class _MyHomePageState extends State<MyHomePage> { 
+  Future<String> _dataFetch() async { // 1
+      await Future.delayed(const Duration(seconds: 1)); 
+      return "データ"; 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final text = FutureBuilder( // 2
+        future: _dataFetch(), // 3
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) { // 4
+          if (snapshot.connectionState == ConnectionState.waiting) { // 5
+            return const CircularProgressIndicator();
+          }
+
+          final data = snapshot.data ?? ""; // 6
+
+          return Text(data); // 7
+        }
+    );
+    return Scaffold(body: text);
+  }
 }
